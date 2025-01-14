a35ae19f5a08d09f8ce358bae08ca32b9a14f86e
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-14 06:22:17 +0530

    Time: 3 ms (75.81%) | Memory: 86.5 MB (61.99%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,21 +1,37 @@
+// class Solution {
+// public:
+//     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+
+//         vector<int> C(A.size(),0);
+//         for(int i=0 ; i<A.size() ;i++){
+//             bool found=false;
+//             for(int j=0 ; j<B.size() ;j++){
+//                 if(A[i]==B[j]){
+//                     found = true;
+//                 }
+//                 if(found && j>=i){
+//                     C[j]++;
+//                 }
+//             }
+//         }
+//         return C;
+        
+        
+//     }
+// };
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-
-        vector<int> C(A.size(),0);
-        for(int i=0 ; i<A.size() ;i++){
-            bool found=false;
-            for(int j=0 ; j<B.size() ;j++){
-                if(A[i]==B[j]){
-                    found = true;
-                }
-                if(found && j>=i){
-                    C[j]++;
-                }
-            }
+        int cnt=0;
+        vector<int>ans;
+        vector<int>freq(51,0);
+        for(int i=0;i<A.size();i++){
+            freq[A[i]]++;
+            if(freq[A[i]]==2)cnt++;
+            freq[B[i]]++;
+            if(freq[B[i]]==2)cnt++;
+            ans.push_back(cnt);
         }
-        return C;
-        
-        
+        return ans;
     }
 };
