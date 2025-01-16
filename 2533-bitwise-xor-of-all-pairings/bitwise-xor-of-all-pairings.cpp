17c384b0a28817105b8bb88258426e4192668f12
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-16 20:19:08 +0530

    Time: 3 ms (12.20%) | Memory: 64.4 MB (36.18%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -1,22 +1,73 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        int ans=0;
-        int m = nums2.size();
-        if(m&1){
-            for (int ele :nums1){
-                ans ^=ele;
+        int c1=nums1.size();
+        int c2=nums2.size();
+        int x1=0 , x2=0;
 
-            }
-
-        }
-        int n=nums1.size();
-        if(n&1){
-            for(int ele:nums2){
-                ans^=ele;
+        if(c1%2 != 0){
+            for(int num:nums2){
+                x2^=num;
             }
         }
-        return ans;
-        
+        if(c2%2 != 0){
+            for(int num:nums1){
+                x1^=num;
+            }
+        }
+        return x1^x2;
     }
 };
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// // public:
+// //     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+// //         int ans=0;
+// //         int m = nums2.size();
+// //         if(m&1){
+// //             for (int ele :nums1){
+// //                 ans ^=ele;
+
+// //             }
+
+// //         }
+// //         int n=nums1.size();
+// //         if(n&1){
+// //             for(int ele:nums2){
+// //                 ans^=ele;
+// //             }
+// //         }
+// //         return ans;
+        
+// //     }
+// // };
+// class Solution {
+// public:
+//     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+//         int ans=0;
+
+//         //Find contribution by first array elements
+//         int m=nums2.size();
+//         if(m&1){
+//             for(int ele: nums1)
+//                 ans^=ele;
+//         }
+
+//         //Find contribution by second array elements
+//         int n=nums1.size();
+//         if(n&1){
+//             for(int ele: nums2)
+//                 ans^=ele;
+//         }
+//         return ans;
+//     }
+// };
