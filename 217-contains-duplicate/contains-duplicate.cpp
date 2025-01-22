5e0ba597fadda546a36d154eec91f3f2c4c68869
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-22 22:53:05 +0530

    Time: 36 ms (37.82%) | Memory: 75.7 MB (11.69%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        //BRUTE FORCE
         // int n=nums.size();
         
         // for(int i=0 ; i<n ; i++){
@@ -12,13 +13,25 @@ public:
         //     }
         // }
         // return false;
-        sort(nums.begin() , nums.end());
-        for(int i=0 ; i<nums.size()-1 ; i++){
-            if (nums[i]==nums[i+1]){
-                return true;
-            }
+        //USING SORT FUNCTION - O(nlogn)
+        // sort(nums.begin() , nums.end());
+        // for(int i=0 ; i<nums.size()-1 ; i++){
+        //     if (nums[i]==nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //Maps
+        unordered_map<int,int> mymap;
+        for(auto a:nums ){
+            mymap[a]++;
         }
-        return false;
+        for(auto a : mymap){
+            if(a.second > 1)        return true;
+        }
+        return 0;
+
         
     }
 };
