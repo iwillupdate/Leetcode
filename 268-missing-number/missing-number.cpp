9b1742cd8a288565f59270d95a1d6f1f2b470c90
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-24 05:48:19 +0530

    Time: 19 ms (8.14%) | Memory: 21.7 MB (88.80%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
-    // Function to find the missing number 
     int missingNumber(vector<int>& nums) {
-       // Calculate N from the size of nums
-        int N = nums.size(); 
-        
-        // Outer loop that runs from 0 to N
-        for (int i = 0; i <= N; i++) {
-            /* Flag variable to check 
-            if an element exists*/
-            int flag = 0;
-            
-            // Search for the element using linear search
-            for (int j = 0; j < N; j++) {
-                if (nums[j] == i) {
-                    // i is present in the array
-                    flag = 1;
-                    break;
-                }
+        sort(nums.begin(),nums.end());
+        for(int i=0 ; i<nums.size() ; i++){
+            if(nums[i]!=i){
+                return i;
             }
-            
-            // Check if the element is missing (flag == 0)
-            if (flag == 0) return i;
         }
+        return nums.size();
         
-        /*  The following line will never
-        execute,it is just to avoid warnings*/
-        return -1;
     }
 };
+
+
+// class Solution {
+// public:
+//     // Function to find the missing number 
+//     int missingNumber(vector<int>& nums) {
+//        // Calculate N from the size of nums
+//         int N = nums.size(); 
+        
+//         // Outer loop that runs from 0 to N
+//         for (int i = 0; i <= N; i++) {
+//             /* Flag variable to check 
+//             if an element exists*/
+//             int flag = 0;
+            
+//             // Search for the element using linear search
+//             for (int j = 0; j < N; j++) {
+//                 if (nums[j] == i) {
+//                     // i is present in the array
+//                     flag = 1;
+//                     break;
+//                 }
+//             }
+            
+//             // Check if the element is missing (flag == 0)
+//             if (flag == 0) return i;
+//         }
+        
+//         /*  The following line will never
+//         execute,it is just to avoid warnings*/
+//         return -1;
+//     }
+// };
