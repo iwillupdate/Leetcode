2ab390b842d64ae773ce8b4155f98ebc1efe7d96
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-25 08:51:46 +0530

    Time: 10 ms (37.40%) | Memory: 128.1 MB (94.26%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size(), 0);
+        int pos=0 , neg=1;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(nums[i]>0){
+                ans[pos]=nums[i];
+                pos += 2;
+            }
+            else{
+                ans[neg]=nums[i];
+                neg += 2;
+            }
+        }
+        return ans;
 
-        int n=nums.size();
-        vector<int> pos , neg;
-        for(int i=0 ;i<n;i++){
-            if(nums[i]>0)       pos.push_back(nums[i]);
-            else neg.push_back(nums[i]);
-        }
-        for(int i=0 ; i<n/2 ; i++){
-            nums[2*i]=pos[i];
-            nums[2*i + 1]=neg[i];
-        }
-        return nums;
+
         
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+
+//         int n=nums.size();
+//         vector<int> pos , neg;
+//         for(int i=0 ;i<n;i++){
+//             if(nums[i]>0)       pos.push_back(nums[i]);
+//             else neg.push_back(nums[i]);
+//         }
+//         for(int i=0 ; i<n/2 ; i++){
+//             nums[2*i]=pos[i];
+//             nums[2*i + 1]=neg[i];
+//         }
+//         return nums;
+        
+//     }
+// };
