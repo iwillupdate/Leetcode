6c765a83c4e280c7d6e5cada38e72a1bbfbb7fda
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-26 00:42:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.07%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,10 @@
+#include<algorithm>
+#include<vector>
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        std::next_permutation(nums.begin(),nums.end());
+        
+        
+    }
+};
