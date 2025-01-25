202b7d56fc0579b19fd07c20e6fc305725e40414
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-25 08:37:35 +0530

    Time: 2 ms (79.44%) | Memory: 14.8 MB (46.90%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,16 +2,38 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mymap;
-        //find the pair element by element
-        for(int i=0 ; i<nums.size();i++){
-            if(mymap.count(target-nums[i])){
-                return {i,mymap[target-nums[i]]};
+        for(int i=0 ; i<nums.size() ; i++){
+            int complement = target-nums[i];
+
+            if(mymap.find(complement)!=mymap.end()){
+                return {mymap[complement],i};
             }
-             //insert element by element
-        mymap[nums[i]]=i;
+            mymap[nums[i]]=i;
+
         }
         return {};
-       
         
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int,int> mymap;
+//         //find the pair element by element
+//         for(int i=0 ; i<nums.size();i++){
+//             if(mymap.count(target-nums[i])){
+//                 return {i,mymap[target-nums[i]]};
+//             }
+//              //insert element by element
+//         mymap[nums[i]]=i;
+//         }
+//         return {};
+       
+        
+//     }
+// };
