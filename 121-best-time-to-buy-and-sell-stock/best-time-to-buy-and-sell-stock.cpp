0ff3aab5b3ef67b06f6e0881e0f52a802e12af89
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-25 08:59:44 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (60.15%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,33 @@
-#include<algorithm>
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max_profit=0;
-        int min_so_far=prices[0];
-        for(int i=0 ; i<prices.size() ; i++){
-            min_so_far = std :: min(min_so_far , prices[i]);
-            int profit = prices[i]-min_so_far;
-            max_profit = std :: max(max_profit , profit);
+        int mini=prices[0];
+        int maxi=0;
+        for(int i=0 ; i<prices.size();i++){
+            if(prices[i]<mini){
+                mini=prices[i];
+            }
+            else{
+                maxi=max(maxi,prices[i]-mini);
+            }
         }
-        return max_profit;
+        return maxi;
         
     }
 };
+
+// #include<algorithm>
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int max_profit=0;
+//         int min_so_far=prices[0];
+//         for(int i=0 ; i<prices.size() ; i++){
+//             min_so_far = std :: min(min_so_far , prices[i]);
+//             int profit = prices[i]-min_so_far;
+//             max_profit = std :: max(max_profit , profit);
+//         }
+//         return max_profit;
+        
+//     }
+// };
