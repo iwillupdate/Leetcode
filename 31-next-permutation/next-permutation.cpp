1e3cf49b01f325d3dcd98bf19915413683979fc7
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-01-27 12:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (67.62%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,10 +1,23 @@
-#include<algorithm>
-#include<vector>
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        std::next_permutation(nums.begin(),nums.end());
-        
-        
+        int i;
+        for(i = nums.size() - 2; i >= 0; i--)
+            if(nums[i] < nums[i + 1])
+                break;
+        if(i >= 0)
+        {
+            int next_bigger = i + 1;
+            for(int j = i + 1; j < nums.size(); j++)
+                if(nums[j] > nums[i] && nums[j] < nums[next_bigger])
+                    next_bigger = j;
+            int temp = nums[i];
+            nums[i] = nums[next_bigger];
+            nums[next_bigger] = temp;
+        }
+        i++;
+        sort(nums.begin() + i, nums.end());
     }
+        
+    
 };
