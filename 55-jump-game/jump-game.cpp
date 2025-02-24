3a52a75f1733c2608661c1dede721488d928d4d2
Vartika Singh <154607046+buzzcodez@users.noreply.github.com>
2025-02-24 19:22:07 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (31.13%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int reach=0;
+        for(int i=0 ; i<nums.size();i++){
+            if(i>reach)     return 0;
+
+            reach=max(reach , nums[i]+i);
+
+
+        }
+        return 1;
+        
+        
+    }
+};
