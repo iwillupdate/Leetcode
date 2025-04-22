d6ce206f82d8ccb4bfca75174fd40625ab9a27e9
Vartika Singh <154607046+vartikasingh117118@users.noreply.github.com>
2025-04-23 02:38:37 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (27.61%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=0;
-        for(int i=0 ; i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
             n=n^nums[i];
         }
         return n;
-        
     }
 };
-//a^a=0
-//a^0=a
