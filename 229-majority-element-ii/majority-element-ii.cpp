b129c8e4ea8fd436ba95a519a080b6cd9b7419d4
Vartika Singh <154607046+vartikasinghp@users.noreply.github.com>
2025-05-12 14:48:35 +0530

    Time: 2300 ms (5.47%) | Memory: 23.4 MB (9.90%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> result;
-        for(int i=0 ;i<nums.size() ;i++){
-            //check if nums[i] is not already part of ans
-            if(result.size()==0 || result[0]!=nums[i]){
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0 ; i<n ;i++){
+            if(ans.size()==0 || ans[0]!=nums[i]) {
                 int cnt=0;
-                for(int j=0 ; j<nums.size();j++){
-                    if(nums[j]==nums[i]){
-                        cnt++;
-                    }
+                for(int j=0 ; j<n ;j++){
+                    if(nums[j]==nums[i]) cnt++;
                 }
-                if(cnt>(nums.size()/3))       result.push_back(nums[i]);
-                if(result.size()==2)        break;
+                if(cnt>(n/3)) ans.push_back(nums[i]);
             }
+            if(ans.size()==2) break;
         }
-        return result;
-
-
+        return ans;
         
     }
 };
