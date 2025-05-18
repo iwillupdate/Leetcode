be98352d405e824e71d51a0e4823b5dcce43cb0d
Vartika Singh <154607046+vartikasinghp@users.noreply.github.com>
2025-05-18 14:03:31 +0530

    Time: 7 ms (22.05%) | Memory: 9.7 MB (48.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()){
-            return false;
-        }
+        if(s.size()!=t.size()) return false;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-
-        for(int i=0 ; i<s.length() ;i++){
-            if(s[i]!=t[i]){
-                return false;
-            }
-
-        }
-        return true;
+        return s==t;
         
     }
 };
