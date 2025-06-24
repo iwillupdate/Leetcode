630972bb45c827fab80cb6e400c26dc416c75c72
Vartika Singh <154607046+vartikasinghp@users.noreply.github.com>
2025-06-25 00:40:47 +0530

    Time: 107 ms (23.67%) | Memory: 16.3 MB (7.89%) - LeetSync

---
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        set<int>result;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==key){
+                int start=max(0,i-k);
+                int end=min(n-1,i+k);
+                for(int j=start;j<=end;j++){
+                    result.insert(j);
+                }
+            }
+        }
+        return vector<int>(result.begin(),result.end());
+    }
+};
